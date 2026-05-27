d729ebeec88c4c74e595dc45c455087ef130ee23
Edgar Barrientos <edgarmbarr@gmail.com>
2026-05-27 11:34:59 -0700

    created non circular PersonNode linked list

--- Josephus/JosephusSim.java
+++ Josephus/JosephusSim.java
@@ -1,59 +1,64 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class JosephusSim {
-   private PersonNode circle;     // a PersonNode pointer that tracks first node
-   private int size;              // the number of people in the circle
-   private int eliminationCount;  // the number to count to for elimination       
-   private PersonNode track;      // a PersonNode pointer to help with elimination
+  private PersonNode circle; // a PersonNode pointer that tracks first node
+  private int size; // the number of people in the circle
+  private int eliminationCount; // the number to count to for elimination
+  private PersonNode track; // a PersonNode pointer to help with elimination
 
-   public JosephusSim(String fileName) {
-      try {
-         // load names from the file in order, generating a singly linked list of PersonNodes
-         Scanner file = new Scanner(new File(fileName));
-         ArrayList<String> list = new ArrayList<String>();
-         
-         // make the ring circular by attaching last node's next to front
-         while(file.hasNextLine()) {
-            String name = file.nextLine();
-            list.add(name);
-            size++;
-         }
-         System.out.println(list);
-         
-         // remember the last node as the one in front of the next to get eliminated
-         
-         // generate, print, and save the random elimination count
+  public JosephusSim(String fileName) {
+    try {
+      // load names from the file in order, generating a singly linked list of PersonNodes
+      Scanner file = new Scanner(new File(fileName));
+      ArrayList<PersonNode> list = new ArrayList<PersonNode>();
 
-      } catch(FileNotFoundException e) {
-         System.out.println("Something went wrong with " + fileName);
+      // make the ring circular by attaching last node's next to front
+      while (file.hasNextLine()) {
+        String name = file.nextLine();
+
+        PersonNode personNode = new PersonNode(name);
+        if (circle != null) {
+          circle.next = personNode;
+        }
+        circle = personNode;
+        list.add(circle);
+
+        size++;
       }
-   }
-   
-   // optional helper method for constructing the circle
-   private void add(String val) {
-   }
-   
-   public void eliminate() {
-      // count to the elimination count
-      
-      // print who will be eliminated
-      
-      // eliminate the person and update "front" of the circle and size
+      System.out.println(list);
 
-   }
-   
-   public boolean isOver() {
-      // check if there's only one person left in the circle
-      return false;
-   }
-   
-   public String toString() {
-      // if there's only one person left, print them as the last survivor
-      
-      // print the remaining survivors (watch out for infinite loop since list is circular)
+      // remember the last node as the one in front of the next to get eliminated
 
-      return "";
-   }
+      // generate, print, and save the random elimination count
 
+    } catch (FileNotFoundException e) {
+      System.out.println("Something went wrong with " + fileName);
+    }
+  }
+
+  // optional helper method for constructing the circle
+  private void add(String val) {}
+
+  public void eliminate() {
+    // count to the elimination count
+
+    // print who will be eliminated
+
+    // eliminate the person and update "front" of the circle and size
+
+  }
+
+  public boolean isOver() {
+    // check if there's only one person left in the circle
+    return false;
+  }
+
+  public String toString() {
+    // if there's only one person left, print them as the last survivor
+
+    // print the remaining survivors (watch out for infinite loop since list is circular)
+
+    return "";
+  }
 }
